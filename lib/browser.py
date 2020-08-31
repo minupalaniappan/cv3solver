197b443a1e740a68fa29bbf253b684aad24a3c45
Minu Palaniappan <minu@middesk.com>
2020-08-30 21:45:40 -0700

    Add more flexibility on browser startup

--- lib/browser.py
+++ lib/browser.py
@@ -2,16 +2,40 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 
-BROWSER = {}
-DRIVER = None
+from flask import Flask, request
+
+app = Flask(__name__)
+
+app.config['PAGES'] = 0
+app.config['DRIVER'] = None
 
 
 def solve(site_key, action, site):
     options = Options()
     options.headless = True
 
+    PAGES = app.config.get('PAGES', None)
+    DRIVER = app.config.get('DRIVER', None)
+
+    if PAGES is 50 and DRIVER is not None:
+        app.config['PAGES'] = 0
+        app.config['DRIVER'] = None
+        DRIVER.quit()
+
     if DRIVER is None:
-        driver = webdriver.Firefox(options=options)
+        app.config['DRIVER'] = webdriver.Firefox(options=options)
+
+    driver = app.config.get('DRIVER', None)
+
+    if driver is None:
+        return ''
+
+    driver.execute_script(
+        '''window.open("{site}", "_blank");'''.format(site=site))
+
+    driver.switch_to.window(driver.window_handles[PAGES])
+
+    app.config['PAGES'] = app.config.get('PAGES', 0) + 1
 
     driver.get(site)
     driver.execute_script(
